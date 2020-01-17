14fc24ab0343eb0a2a1a0342f6e8f778bdf59b7d
devmqk <achraf.kh03@gmail.com>
2020-01-17 13:55:05 +0100

    second Commit

--- lib/pharm.dart
+++ lib/pharm.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-//import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'models/global.dart';
 import 'models/Pharmacie.dart';
