05077f984d5ceeb1efcf5ae689e4beb6a40ddd6f
devmqk <achraf.kh03@gmail.com>
2020-01-17 13:56:07 +0100

    second Commit

--- lib/models/global.dart
+++ lib/models/global.dart
@@ -5,10 +5,10 @@ Color lightGreen = Color(0xFF95E08E);
 Color lightBlueIsh = Color(0xFF33BBB5);
 Color darkGreen = Color(0xFF00AA12);
 Color backgroundColor = Color(0xFFEFEEF5);
-
+//
 
 TextStyle titleStyleWhite = new TextStyle(
-  fontFamily: 'Helvetica', 
+  fontFamily: 'Helvetica',
   color: Colors.white, 
   fontWeight: FontWeight.bold, 
   fontSize: 25
